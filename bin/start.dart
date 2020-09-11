4b90bc2806256f14952143f76d47c34d07c2f80d
Nico Feulner <s178122@hft-leipzig.de>
2020-09-11 06:10:30 +0200

    Adapt server to recent changes in client

--- bin/start.dart
+++ bin/start.dart
@@ -5,101 +5,49 @@ import 'dart:isolate';
 import 'package:AndroidPushNotificationServer/android_push_notification_server.dart';
 import 'package:tuple/tuple.dart';
 
-void main() async {
-  (await NotificationRunner.initializeRunner(12345))._waitForInput();
+void main() {
+  NotificationRunner.initializeRunner(12345).then((runner) => runner._waitForInput());
 }
 
 class NotificationRunner {
 
   ReceivePort _mainPort;
-  PushNotificationServer _server;
 
-  final Map<String, Tuple2<String, Function(NotificationRunner, [String, String, String, String])>> _options = {
-    'a': Tuple2('Send message to all', (runner, [title, message, uri, icon]) {
-      print('Sending message to ${runner._server.clientCount()} clients');
-      runner._server.sendPushNotification(
-        title,
-        message,
-        uri: uri,
-        icon: icon
-      );
-      runner._waitForInput();
-    }),
-    'i': Tuple2('Send message to it subscribers', (runner, [title, message, uri, icon]) {
-      String category = 'it';
-      print('Sending message to ${runner._server.clientCount(category)} clients');
-      runner._server.sendPushNotification(
-          title,
-          message,
-          uri: uri,
-          icon: icon,
-          category: category
-      );
-      runner._waitForInput();
-    }),
-    's': Tuple2('Send message to sports subscribers', (runner, [title, message, uri, icon]) {
-      String category = 'sports';
-      print('Sending message to ${runner._server.clientCount(category)} clients');
-      runner._server.sendPushNotification(
-          title,
-          message,
-          uri: uri,
-          icon: icon,
-          category: category
-      );
-      runner._waitForInput();
-    }),
-    'f': Tuple2('Send message to finance subscribers', (runner, [title, message, uri, icon]) {
-      String category = 'finance';
-      print('Sending message to ${runner._server.clientCount(category)} clients');
-      runner._server.sendPushNotification(
-          title,
-          message,
-          uri: uri,
-          icon: icon,
-          category: category
-      );
-      runner._waitForInput();
-    }),
-    'x': Tuple2('Exit', (runner, [title, message, uri, icon]) {
-      runner._server.shutDown();
-      runner._mainPort.close();
-    })
+  static final Map<String, dynamic> _options = {
+    's': Tuple2('Send message', [
+      Tuple3('p', 'target package', false),
+      Tuple3('c', 'target channel', false),
+      Tuple3('t', 'notification title', true),
+      Tuple3('m', 'notification message', true),
+      Tuple3('u', 'URI that\'s opened when notification is clicked by the user', false),
+      Tuple3('i', 'icon that\'s shown in the notification (base64 encoded)', false)
+    ]),
+    'x': 'Exit'
   };
 
   NotificationRunner._(PushNotificationServer server) {
-    _server = server;
+    print('This is the server related to the push notification test app.');
     _mainPort = ReceivePort();
     _mainPort.listen((input) {
       if (input is SendPort) {
-        input.send(_options.keys.join(','));
-      } else if (input == _options.keys.last) {
-        _options[input].item2(this);
-      } else {
-        Map<String, dynamic> inputs = jsonDecode(input);
-        String uri = inputs['u'];
-        String icon = inputs['i'];
-        _options[inputs['o']].item2(
-          this,
-          inputs['t'],
-          inputs['m'],
-          uri.isEmpty ? null : uri,
-          icon.isEmpty ? null : icon
+        input.send(_options);
+      } else if (input is String) {
+        if (input == _options.keys.last) {
+          server.shutDown();
+          _mainPort.close();
+        }
+      } else if (input is Map<String, String>) {
+        server.sendPushNotification(
+            input['t'],
+            input['m'],
+            uri: input['u'],
+            icon: input['i'],
+            channel: input['c'],
+            package: input['p']
         );
+        _waitForInput();
       }
     });
-    print('This is the server related to the push notification test app. '
-        'You will have following options to choose from when you are '
-        'prompted to input something.');
-    _options.forEach((key, tuple) {
-      print('$key: ${tuple.item1}');
-    });
-    print('Unless you input ${_options.keys.last}, you will have to '
-        'input a title followed by a message right after. Then you will '
-        'have the option to either input a URI that will be handled as an '
-        'intent by the receivers or just press enter to skip the uri '
-        'input. Finally you can optionally input a base64 encoded icon '
-        'or just press enter again to make the client show the default icon.');
   }
 
   void _waitForInput() {
@@ -111,10 +59,7 @@ class NotificationRunner {
 
   static Future<NotificationRunner> initializeRunner(int port) async {
     return NotificationRunner._(
-        await PushNotificationServer.initializeServer(
-            port,
-            <String>['it', 'finance', 'sports']
-        )
+        await PushNotificationServer.initializeServer(port)
     );
   }
 }
@@ -122,31 +67,49 @@ class NotificationRunner {
 
 void isolatedInput(SendPort message) {
   ReceivePort isolatePort = ReceivePort();
-  isolatePort.listen((allowedInputs) {
-    List<String> allowedInputsList = allowedInputs.split(',');
-    String option = getInput(allowedInputsList);
-    if (option == allowedInputsList.last) {
-      message.send(option);
+  isolatePort.listen((options) {
+    String selection = getMainInput(options);
+    dynamic selectionValue = options[selection];
+    if (selectionValue is String) {
+      message.send(selection);
     } else {
-      message.send(jsonEncode(<String, String>{
-        'o': option,
-        't': getInput(),
-        'm': getInput(),
-        'u': getInput(),
-        'i': getInput()
-      }));
+      Map<String, String> ret = Map<String, String>();
+      (selectionValue.item2 as List<Tuple3>).forEach((tuple3) {
+        String input = getInput(tuple3);
+        if (input.length > 0) {
+          ret[tuple3.item1] = input;
+        }
+      });
+      message.send(ret);
     }
     isolatePort.close();
   });
   message.send(isolatePort.sendPort);
 }
 
-String getInput([List<String> allowedInputs]) {
-  print('Please input something:');
+String getInput(Tuple3 tuple3) {
+  if (!tuple3.item3) {
+    print('The following selection is optional. '
+        'If you do not want to specify it, just press enter.');
+  }
+  print('Please type your desired ${tuple3.item2}:');
   String input = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
-  if (allowedInputs != null && !allowedInputs.contains(input)) {
+  if (tuple3.item3 && input.length == 0) {
+    print('This selection is not optional. Try again...');
+    getInput(tuple3);
+  }
+  return input;
+}
+
+String getMainInput(Map<String, dynamic> allowedInputs) {
+  print('You have following options:');
+  allowedInputs.forEach((key, value) {
+    print('$key: ' + (value is Tuple2 ? value.item1 : value));
+  });
+  String input = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+  if (!allowedInputs.keys.contains(input)) {
     print('Only the following inputs are allowed: $allowedInputs\nTry again...');
-    return getInput(allowedInputs);
+    return getMainInput(allowedInputs);
   }
   return input;
 }
--- lib/android_push_notification_server.dart
+++ lib/android_push_notification_server.dart
@@ -9,59 +9,91 @@ import 'package:tuple/tuple.dart';
 
 class PushNotificationServer {
   ServerSocket _serverSocket;
-  List<Tuple2<Socket, List<String>>> _sockets;
-  List<String> _categories;
+  List<Tuple2<Socket, Map<String, List<String>>>> _sockets;
 
   void shutDown() async {
-    for (Tuple2<Socket, List<String>> socket in _sockets) {
+    for (Tuple2<Socket, Map<String, List<String>>> socket in _sockets) {
       await socket.item1.close();
     }
     await _serverSocket.close();
   }
 
-  Iterable<Tuple2<Socket, List<String>>> _getSockets([String category]) {
-    return category == null
-        ? _sockets
-        : _sockets.where((socket) => socket.item2.contains(category)).toList();
+  Iterable<Tuple2<Socket, Map<String, List<String>>>> _getSockets([String package, String channel]) {
+    if (package == null) {
+      return _sockets;
+    }
+    Iterable<Tuple2<Socket, Map<String, List<String>>>> sockets = _sockets
+        .where((socket) => socket.item2.containsKey(package));
+    if (channel == null) {
+      return sockets;
+    }
+    return sockets.where((socket) => socket.item2[package].contains(channel));
   }
 
-  int clientCount([String category]) {
-    return _getSockets(category).length;
+  int clientCount([String package, String channel]) {
+    return _getSockets(package, channel).length;
   }
 
-  void sendPushNotification(String title, String message,
-      {String uri, String icon, String category}) {
-    Map<String, dynamic> pushNotification = {
+  void sendPushNotification(String title, String message, {String uri, String icon, String package, String channel}) {
+    Map<String, dynamic> notification = {
       'title': title,
       'message': message
     };
     <String, String>{'uri': uri, 'icon': icon}.forEach((name, param) {
       if (param != null) {
-        pushNotification[name] = param;
+        notification[name] = param;
       }
     });
-    _getSockets(category).forEach(
-        (socket) => socket.item1.writeln(jsonEncode(pushNotification)));
+    Map<String, Map<String, dynamic>> pushNotification = {
+      'push_notification': notification
+    };
+    _getSockets(package, channel).forEach(
+        (socket) => socket.item1.writeln(jsonEncode(pushNotification))
+    );
   }
 
-  PushNotificationServer._(ServerSocket serverSocket,
-      [List<String> categories]) {
-    _categories = categories == null ? <String>[] : categories;
+  PushNotificationServer._(ServerSocket serverSocket) {
     _serverSocket = serverSocket;
-    _sockets = <Tuple2<Socket, List<String>>>[];
+    _sockets = <Tuple2<Socket, Map<String, List<String>>>>[];
     _serverSocket.listen((Socket socket) {
-      Tuple2<Socket, List<String>> socketEntry = Tuple2(socket, <String>[]);
+      Tuple2<Socket, Map<String, List<String>>> socketEntry = Tuple2(socket, <String, List<String>>{});
       _sockets.add(socketEntry);
       socket.listen((Uint8List data) {
-        String category = String.fromCharCodes(data);
-        category = category.substring(0, category.length - 1);
-        List<String> clientCategories = socketEntry.item2;
-        if (_categories.contains(category)) {
-          if (!clientCategories.contains(category)) {
-            clientCategories.add(category);
-          }
-        } else {
-          _finishSocket(socketEntry);
+        String message = String.fromCharCodes(data);
+        message = message.substring(0, message.length - 1);
+        Map<String, dynamic> rootJson = jsonDecode(message);
+        switch (rootJson.keys.first) {
+          case 'application_subscription':
+            String package = rootJson['application_subscription']['package'];
+            Map<String, List<String>> packages = socketEntry.item2;
+            if (package != null && !packages.containsKey(package)) {
+              packages[package] = <String>[];
+            }
+            break;
+          case 'channel_subscription':
+            Map<String, String> json = rootJson['channel_subscription'];
+            String package = json['package'];
+            String channel = json['channel'];
+            Map<String, List<String>> packageChannels = socketEntry.item2;
+            if (package != null && packageChannels.containsKey(package) && channel != null) {
+              List<String> channels = packageChannels[package];
+              if (!channels.contains(channel)) {
+                channels.add(channel);
+              }
+            } else {
+              _finishSocket(socketEntry);
+            }
+            break;
+          case 'clients':
+            try {
+              socketEntry.item2.addAll(rootJson['clients']);
+            } on TypeError {
+              _finishSocket(socketEntry);
+            }
+            break;
+          default:
+            _finishSocket(socketEntry);
+            break;
         }
       }, onDone: () {
         _finishSocket(socketEntry);
@@ -71,13 +103,13 @@ class PushNotificationServer {
     });
   }
 
-  static Future<PushNotificationServer> initializeServer(
-      int port, List<String> categories) async {
+  static Future<PushNotificationServer> initializeServer(int port) async {
     return PushNotificationServer._(
-        await ServerSocket.bind(InternetAddress.anyIPv4, port), categories);
+        await ServerSocket.bind(InternetAddress.anyIPv4, port)
+    );
   }
 
-  void _finishSocket(Tuple2<Socket, List<String>> socketEntry) {
+  void _finishSocket(Tuple2<Socket, Map<String, List<String>>> socketEntry) {
     try {
       socketEntry.item1.close();
     } on SocketException {}
