f083838c4ca1f19f471f14561b104b36df399b1a
Nico Feulner <s178122@hft-leipzig.de>
2020-09-12 19:55:10 +0200

    Fix test

--- test/android_push_notification_server_test.dart
+++ test/android_push_notification_server_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   test('initializeServer', () async {
-    PushNotificationServer server = await PushNotificationServer.initializeServer(12345, []);
+    PushNotificationServer server = await PushNotificationServer.initializeServer(12345);
     expect(server.clientCount(), 0);
     server.shutDown();
   });
